b13879642baa5344a09d48875b2c98506b67227a
MrCocomass <daniel_miranda_apps1ma1718@cev.com>
2019-10-28 14:41:30 +0100

    Initial commit

---
+++ app/Password.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class password extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'password',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
